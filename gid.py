8efa3a368b4ac36bec5fe122363280d74c93a1da
Jacob Sherwood <sport@fizzix.org>
2015-03-02 14:04:17 -0500

    Initial creation of gid.

---
+++ gid.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+import cgi
+import cgitb
+cgitb.enable()
+
+print "Content-type: text/html"
+print
+print "<BODY>"
+print "<TITLE>Hello, World</TITLE>"
+print "<H1>Hello, World</H1>"
+print "</BODY>"
